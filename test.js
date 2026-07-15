d3428c4fbb2de25d3e3f9b8b9e1cc1d063454dec
ramawaterfilter <github@ramawaterfilter.com>
2026-07-15 15:26:54 +0530

    - Added new CSS file to style subscription elements and improve layout for Gravity Water Filter product.

---
+++ test.js
@@ -0,0 +1,26 @@
+﻿<!-- Wistia player script -->
+<script src="https://fast.wistia.com/player.js" async></script>
+
+<!-- Video embed script -->
+<script
+  src="https://fast.wistia.com/embed/oon8hhww93.js"
+  async
+  type="module"
+></script>
+
+<!-- Placeholder shown while the Wistia player loads -->
+<style>
+  wistia-player[media-id='oon8hhww93']:not(:defined) {
+    background: center / contain no-repeat
+      url('https://fast.wistia.com/embed/medias/oon8hhww93/swatch');
+    display: block;
+    filter: blur(5px);
+    padding-top: 56.25%;
+  }
+</style>
+
+<!-- Video player -->
+<wistia-player
+  media-id="oon8hhww93"
+  aspect="1.7777777777777777"
+></wistia-player>
